67be9b0706eb00c4cf0f536ab0821c78a9ac5ad9
Arnau Feliu <feliu.arnau@gmail.com>
2022-10-22 23:56:04 +0200

    minor changes

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,24 +1,34 @@
-import {createLogger, format, transports } from "winston";
-const { combine, printf, label, timestamp, colorize, simple } = format;
+import winston from "winston";
 
-const customLogFormat = () =>{
-    const myFormat = printf(({ level, message, label, timestamp }) => {
-        return `${timestamp} [${label}] ${level}: ${message}`;
-    });
-    return combine(label({ label: 'main-logger' }), timestamp(), myFormat)
-}
+const myFormat = winston.format.printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
 
-const logger = createLogger({
+const logger = winston.createLogger({
     level: 'info',
-    format: customLogFormat(),
+    format: winston.format.combine(
+      winston.format.label({ label: 'app' }),
+      winston.format.timestamp(),
+      winston.format.simple(),
+      myFormat
+    ),
+    defaultMeta: { service: 'user-service' },
     transports: [
-      new transports.File({ filename: 'error.log', level: 'error' }),
-      new transports.File({ filename: 'combined.log' }),
+      new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
+      new winston.transports.File({ filename: './logs/combined.log' }),
     ],
   });
 
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new transports.Console({format}));
-}
+  if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.label({ label: 'app' }),
+        winston.format.timestamp(),
+        winston.format.json(),
+        myFormat
+      )
+    }));
+  }
 
 export default logger;
--- src/process.js
+++ src/process.js
@@ -1,12 +1,13 @@
 import { connection } from "./config/redis.js";
 import { Queue, Worker } from 'bullmq'
 import { v4 as uuidv4 } from 'uuid';
+import logger from "./config/logger.js";
 
 const queue = new Queue('messages', { connection });
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 100; i++) {
     const data = { num: i, id: uuidv4() }
     const options = { removeOnComplete: true, removeOnFail: true, delay: 0, attempts: 0 }
-    console.log("Added: ",i)
+    logger.info(`Message ${i} add to queue`);
     await queue.add("message", data, options )
 }
